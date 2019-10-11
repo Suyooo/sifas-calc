23150113b37448a527188fb5611d5e7e1a2e10a5
Suyooo <superyoshi1@gmx.de>
2019-10-11 16:30:40 +0200

    Fix errors on invalid inputs and SP sorter with less than three units

--- js/sppower.js
+++ js/sppower.js
@@ -40,9 +40,16 @@ function sortAllUnits() {
     if (units.length < 3) {
         topunits = units;
         var boosts = 0;
-        if (units.length >= 1 && units[0].match) boosts++;
-        if (units.length >= 2 && units[1].match) boosts++;
-        topsp = (units[0].power + units[1].power + units[2].power) * getRateFromNumberOfBoosts(boosts);
+        var sum = 0;
+        if (units.length >= 1) {
+            sum += units[0].power;
+            if (units[0].match) boosts++;
+        }
+        if (units.length >= 2) {
+            sum += units[1].power;
+            if (units[1].match) boosts++;
+        }
+        topsp = sum * getRateFromNumberOfBoosts(boosts);
     } else if (sortattr === 0) {
         topunits = [units[0], units[1], units[2]];
         topsp = (units[0].power + units[1].power + units[2].power) * getRateFromNumberOfBoosts(3);
