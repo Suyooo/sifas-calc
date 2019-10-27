9db9efdd548f5a0311a3620b033747d8a0781fee
Suyooo <superyoshi1@gmx.de>
2019-10-27 07:07:38 +0100

    Change sleep warning message

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -2,7 +2,7 @@ var jpDateOverride = [new Date(Date.UTC(2019, 9, 21, 6)),
     new Date(Date.UTC(2019, 9, 31, 6))];
 
 var notificationInfo = {
-    message: "<b>October 18th Update:</b> The new LP recharge times/costs/rewards are now in effect.",
-    id: 3,
-    allowDismiss: true
+    message: "<b>Event Point Rewards have changed and are not correct anymore.</b> This will take a while to fix. In the meantime, the results of the calc are not even close to accurate. If you have any screenshots of your event point rewards (especially lower ranks!), please send them to me over Discord or GitHub (links in the side menu) - thank you so much!",
+    id: 4,
+    allowDismiss: false
 };
