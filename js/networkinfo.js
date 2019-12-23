efe0d8a6bae82c288ae7e8f17395f4dc3e387996
Suyooo <superyoshi1@gmx.de>
2019-12-23 15:34:09 +0100

    Add Hard+ difficulty

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,8 +1,8 @@
-var jpDateOverride = [new Date(Date.UTC(2019, 10, 6, 6)),
-    new Date(Date.UTC(2019, 10, 15, 6))];
+var jpDateOverride = [new Date(Date.UTC(2019, 11, 23, 6)),
+    new Date(Date.UTC(2019, 11, 31, 6))];
 
 var notificationInfo = {
-    message: "<b>November 5 Update:</b> You can now specify the amount of hours you want to sleep at night in the Timer section. The calculator will take LP lost to extra sleep into account.",
+    message: "<b>December 23 Update:</b> Hard+ has been added to the difficulty selection (D/C/B/A/S Rank EP rewards are 815/830/845/860/875, which makes the EP per LP of a D Rank Hard+ better than a S Rank Hard). Happy Holidays!",
     id: 6,
     allowDismiss: true
 };
