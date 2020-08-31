634210f324a275983e107bc286798d9df10f1c73
Suyooo <me@suyo.be>
2020-08-31 19:26:59 +0200

    Fix the fix for colouring when navigating via keyboard

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -4,7 +4,7 @@ var enDateOverride = [new Date(Date.UTC(2020, 7, 20, 6)),
     new Date(Date.UTC(2020, 7, 30, 6))];
 
 var notificationInfo = {
-    message: "<b>March 19 Update:</b> New JP LP cap has been added, and the point tables on WW now use the regular ones. All of this is now controlled through the region selection.<br>Stay healthy, stay home (if it's possible for you), wash your hands and grind responsibly ❤︎",
-    id: 11,
+    message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every. single. note.",
+    id: 12,
     allowDismiss: true
 };
