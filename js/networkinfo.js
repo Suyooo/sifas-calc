3b4e7d022c353aba64e53571a098021bd3fe1d39
Suyooo <me@suyo.be>
2020-09-04 14:21:59 +0200

    Update Klab Warning

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,7 +1,7 @@
-var jpDateOverride = [new Date(Date.UTC(2020, 7, 21, 6)),
-    new Date(Date.UTC(2020, 7, 31, 6))];
-var enDateOverride = [new Date(Date.UTC(2020, 7, 20, 6)),
-    new Date(Date.UTC(2020, 7, 30, 6))];
+var jpDateOverride = [new Date(Date.UTC(2020, 8, 7, 6)),
+    new Date(Date.UTC(2020, 8, 15, 6))];
+var enDateOverride = [new Date(Date.UTC(2020, 8, 4, 6)),
+    new Date(Date.UTC(2020, 8, 9 , 6))];
 
 var notificationInfo = {
     message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every. single. note.",
