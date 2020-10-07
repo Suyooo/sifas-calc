d4f6f56b488f337ff0bd3fcbd9b3c35bacb14f5b
Suyooo <me@suyo.be>
2020-10-07 13:44:08 +0200

    For 1st Anniversary, I finally noticed a big Find+Replace fail LMAO

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,10 +1,10 @@
-var jpDateOverride = [new Date(Date.UTC(2020, 8, 7, 6)),
-    new Date(Date.UTC(2020, 8, 15, 6))];
-var enDateOverride = [new Date(Date.UTC(2020, 8, 4, 6)),
-    new Date(Date.UTC(2020, 8, 9 , 6))];
+var jpDateOverride = [new Date(Date.UTC(2020, 9, 7, 6)),
+    new Date(Date.UTC(2020, 9, 15, 6))];
+var enDateOverride = [new Date(Date.UTC(2020, 9, 3, 6)),
+    new Date(Date.UTC(2020, 9, 9, 6))];
 
 var notificationInfo = {
-    message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every. single. note.",
+    message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every single note.",
     id: 12,
     allowDismiss: true
 };
