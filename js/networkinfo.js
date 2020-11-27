d680a5b32f20a5210d94d0e8a085636299be75c9
Suyooo <me@suyo.be>
2020-11-27 23:52:12 +0100

    Guess who never added the WW banner to the service worker exceptions for 9 months. Oops

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,7 +1,7 @@
-var jpDateOverride = [new Date(Date.UTC(2020, 9, 7, 6)),
-    new Date(Date.UTC(2020, 9, 15, 6))];
-var enDateOverride = [new Date(Date.UTC(2020, 9, 3, 6)),
-    new Date(Date.UTC(2020, 9, 9, 6))];
+var jpDateOverride = [new Date(Date.UTC(2020, 10, 22, 6)),
+    new Date(Date.UTC(2020, 10, 30, 6))];
+var enDateOverride = [new Date(Date.UTC(2020, 10, 25, 6)),
+    new Date(Date.UTC(2020, 11, 1, 6))];
 
 var notificationInfo = {
     message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every single note.",
