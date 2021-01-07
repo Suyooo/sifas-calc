debef46cc9499036010b5963a2c69819f6fe7e8c
Suyooo <me@suyo.be>
2021-01-07 12:11:36 +0100

    Apparently there was a Klab apology. With that, it looking like the main game team is working with the WW team a little more and some big players quitting... I think it's time to retire the Klab warning again. I bet it's getting annoying to WW players. However, I'm leaving the code in this time. Just in case.

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,7 +1,7 @@
-var jpDateOverride = [new Date(Date.UTC(2020, 11, 7, 6)),
-    new Date(Date.UTC(2020, 11, 14, 6))];
-var enDateOverride = [new Date(Date.UTC(2020, 11, 5, 6)),
-    new Date(Date.UTC(2020, 11, 10, 6))];
+var jpDateOverride = [new Date(Date.UTC(2021, 0, 7, 6)),
+    new Date(Date.UTC(2021, 0, 14, 6))];
+var enDateOverride = [new Date(Date.UTC(2021, 0, 5, 6)),
+    new Date(Date.UTC(2021, 0, 11, 6))];
 
 var notificationInfo = {
     message: "<b>NEW AND SHINY:</b> Check out the <a href='/sifas/mapdb'>Note Map Database, an extensive song information page</a> - including not only gimmicks and AC missions, but rewards, gimmick locations, and data on every single note.",
