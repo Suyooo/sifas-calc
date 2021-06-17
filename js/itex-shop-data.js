060d961e7375523cca75f9c4318f39e8c4a3e8a7
Suyooo <me@suyo.be>
2021-06-17 16:30:03 +0200

    Add ItEx shop data

--- js/itex-shop-data.js
+++
@@ -1,5 +0,0 @@
-var itexShop = [
-    {
-        "category": "UR School Idols"
-    }
-]
---
+++ js/itex-shop.js
@@ -0,0 +1,573 @@
+var ITEX_SHOP = [
+    {
+        "category": "School Idols",
+        "items": [
+            {
+                "name": "UR",
+                "amount": 1,
+                "cost": 34500
+            },
+            {
+                "name": "UR (LB1)",
+                "amount": 1,
+                "cost": 400000
+            },
+            {
+                "name": "UR (LB2)",
+                "amount": 1,
+                "cost": 600000
+            },
+            {
+                "name": "SR1",
+                "amount": 1,
+                "cost": 7200
+            },
+            {
+                "name": "SR2",
+                "amount": 1,
+                "cost": 7200
+            },
+            {
+                "name": "SR2 (LB1/2)",
+                "amount": 2,
+                "cost": 12000
+            },
+            {
+                "name": "SR2 (LB3/4/5)",
+                "amount": 3,
+                "cost": 70000
+            }
+        ]
+    }, {
+        "category": "Background",
+        "items": [
+            {
+                "name": "Background",
+                "amount": 1,
+                "cost": 4500
+            }
+        ]
+    }, {
+        "category": "Loveca Stars",
+        "items": [
+            {
+                "name": "Loveca Star x100",
+                "amount": 1,
+                "cost": 10500
+            }
+        ]
+    }, {
+        "category": "High Memory Tickets",
+        "items": [
+            {
+                "name": "High Memory Ticket",
+                "amount": 5,
+                "cost": 2500
+            }
+        ]
+    }, {
+        "category": "School Idol Wishes",
+        "items": [
+            {
+                "name": "Wish",
+                "amount": 30,
+                "cost": 20000
+            }
+        ]
+    }, {
+        "category": "Accessories",
+        "items": [
+            {
+                "name": "First",
+                "amount": 1,
+                "cost": 5000
+            },
+            {
+                "name": "Second to Sixth",
+                "amount": 5,
+                "cost": 10000
+            }
+        ]
+    }, {
+        "category": "Macarons",
+        "items": [
+            {
+                "name": "Red 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Red 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Red 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Blue 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Blue 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Blue 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Green 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Green 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Green 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Yellow 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Yellow 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Yellow 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Purple 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Purple 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Purple 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Pink 1* Macaron x10",
+                "amount": 100,
+                "cost": 150
+            },
+            {
+                "name": "Pink 2* Macaron x10",
+                "amount": 80,
+                "cost": 300
+            },
+            {
+                "name": "Pink 3* Macaron x10",
+                "amount": 80,
+                "cost": 450
+            },
+            {
+                "name": "Silver 1* Macaron x10",
+                "amount": 100,
+                "cost": 200
+            },
+            {
+                "name": "Silver 2* Macaron x10",
+                "amount": 80,
+                "cost": 350
+            },
+            {
+                "name": "Silver 3* Macaron x10",
+                "amount": 80,
+                "cost": 500
+            },
+            {
+                "name": "Gold 1* Macaron x10",
+                "amount": 100,
+                "cost": 200
+            },
+            {
+                "name": "Gold 2* Macaron x10",
+                "amount": 80,
+                "cost": 350
+            },
+            {
+                "name": "Gold 3* Macaron x10",
+                "amount": 80,
+                "cost": 500
+            }
+        ]
+    }, {
+        "category": "Memories",
+        "items": [
+            {
+                "name": "Honoka Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Eli Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Kotori Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Umi Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Rin Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Maki Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Nozomi Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Hanayo Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Nico Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Chika Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Riko Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Kanan Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Dia Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "You Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Yohane Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Hanamaru Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Mari Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Ruby Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Ayumu Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Kasumi Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Shizuku Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Karin Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Ai Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Kanata Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Setsuna Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Emma Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Rina Memory x5",
+                "amount": 1,
+                "cost": 120
+            },
+            {
+                "name": "Shioriko Memory x5",
+                "amount": 1,
+                "cost": 120
+            }
+        ]
+    }, {
+        "category": "Books",
+        "items": [
+            {
+                "name": "Vo 1* Book",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Vo 2* Book",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Vo 3* Book",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Sp 1* Book",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Sp 2* Book",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Sp 3* Book",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Gd 1* Book",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Gd 2* Book",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Gd 3* Book",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Sk 1* Book",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Sk 2* Book",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Sk 3* Book",
+                "amount": 90,
+                "cost": 350
+            }
+        ]
+    }, {
+        "category": "Plants",
+        "items": [
+            {
+                "name": "Vo 1* Plant",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Vo 2* Plant",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Vo 3* Plant",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Sp 1* Plant",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Sp 2* Plant",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Sp 3* Plant",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Gd 1* Plant",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Gd 2* Plant",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Gd 3* Plant",
+                "amount": 90,
+                "cost": 350
+            },
+            {
+                "name": "Sk 1* Plant",
+                "amount": 90,
+                "cost": 150
+            },
+            {
+                "name": "Sk 2* Plant",
+                "amount": 90,
+                "cost": 250
+            },
+            {
+                "name": "Sk 3* Plant",
+                "amount": 90,
+                "cost": 350
+            }
+        ]
+    }, {
+        "category": "School Idol Certificate",
+        "items": [
+            {
+                "name": "Badge",
+                "amount": 10,
+                "cost": 1200
+            }
+        ]
+    }, {
+        "category": "School Idol Radiance",
+        "items": [
+            {
+                "name": "Radiance",
+                "amount": 3,
+                "cost": 7200
+            }
+        ]
+    }, {
+        "category": "Live Items",
+        "items": [
+            {
+                "name": "Skip Ticket x5",
+                "amount": 6,
+                "cost": 700
+            },
+            {
+                "name": "50LP Live Candy",
+                "amount": 2,
+                "cost": 700
+            },
+            {
+                "name": "100LP Live Candy",
+                "amount": 2,
+                "cost": 1400
+            }
+        ]
+    }, {
+        "category": "Training Items",
+        "items": [
+            {
+                "name": "Bulb",
+                "amount": 20,
+                "cost": 3600
+            },
+            {
+                "name": "Lucky Charm",
+                "amount": 20,
+                "cost": 3600
+            }
+        ]
+    }, {
+        "category": "Other",
+        "items": [
+            {
+                "name": "G x5000",
+                "amount": 50,
+                "cost": 450
+            },
+            {
+                "name": "EXP x5000",
+                "amount": 50,
+                "cost": 450
+            },
+            {
+                "name": "G x10",
+                "amount": 1000,
+                "cost": 10
+            },
+            {
+                "name": "EXP x10",
+                "amount": 1000,
+                "cost": 10
+            },
+            {
+                "name": "G x1",
+                "amount": 1000,
+                "cost": 1
+            },
+            {
+                "name": "EXP x1",
+                "amount": 1000,
+                "cost": 1
+            }
+        ]
+    }
+]
