9cc2e5b64c6577078188a219075e416b1016faa9
Suyooo <me@suyo.be>
2021-08-04 12:44:32 +0200

    Fix update script

--- tools/update-server-side-files.sh
+++ tools/update-server-side-files.sh
@@ -2,9 +2,7 @@
 
 echo "Event and Notification Info..."
 uglifyjs -m -c -o build/js/networkinfo.js js/networkinfo.js
-echo "WW Banner..."
-cp image/event_en.jpg build/image/event_en.jpg
-echo "JP Banner..."
-cp image/event_jp.jpg build/image/event_jp.jpg
+echo "Banner..."
+cp image/event.jpg build/image/event_jp.jpg
 
 echo "Done."
