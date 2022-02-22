7d41190f8bffc333175736c60aa8a05696b22790
Suyooo <me@suyo.be>
2022-02-22 09:44:34 +0100

    Add canonical tags so search engines stop complaining

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
+var dateOverride = [new Date(Date.UTC(2022, 1, 20, 6)),
+    new Date(Date.UTC(2022, 1, 27, 6))];
+=======
 var dateOverride = [new Date(Date.UTC(2021, 7, 22, 6)),
     new Date(Date.UTC(2021, 7, 30, 6))];
+>>>>>>> origin/master
 
 var notificationInfo = {
     message: "<b>August 3 Update: </b>Happy Sync. Removing the region select here, too, but I trust Klab WW 0% that there won't be differences down the line lol",
