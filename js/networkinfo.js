79c645967303395578cc1b5ec6cec147a589dde1
Suyooo <me@suyo.be>
2022-06-02 10:32:25 +0200

    Fix incorrect filenames in SP Power calc

--- js/networkinfo.js
+++ js/networkinfo.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-var dateOverride = [new Date(Date.UTC(2022, 1, 20, 6)),
-    new Date(Date.UTC(2022, 1, 27, 6))];
-=======
-var dateOverride = [new Date(Date.UTC(2021, 7, 22, 6)),
-    new Date(Date.UTC(2021, 7, 30, 6))];
->>>>>>> origin/master
+var dateOverride = [new Date(Date.UTC(2022, 4, 7, 6)),
+    new Date(Date.UTC(2022, 4, 14, 6))];
 
 var notificationInfo = {
     message: "<b>August 3 Update: </b>Happy Sync. Removing the region select here, too, but I trust Klab WW 0% that there won't be differences down the line lol",
