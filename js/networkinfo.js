6c19681874f72bafd320590fceedaebffe12524a
Suyooo <me@suyo.be>
2023-05-30 09:21:09 +0200

    bye

---
+++ js/networkinfo.js
@@ -0,0 +1,7 @@
+var dateOverride = [new Date(1569477600000), new Date(1688108400000)];
+
+var notificationInfo = {
+    message: "<b>May 30 Update: </b>The last event is over. But as you can see, the site will remain online for now - so feel free to still play with the numbers or the tools :)",
+    id: 17,
+    allowDismiss: true
+};
